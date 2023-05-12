5e123882bf1b755c774a8471756130c7b502d587
irg1008 <ivansudevlop@gmail.com>
2023-05-13 00:26:36 +0200

    👌 UPDATE: Search updated on search params for refresh persist

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,19 +4,21 @@ import { Autocomplete } from '@/components/ui/autocomplete';
 import { scrollToTop } from '@/lib/utils/window.utils';
 import { $, component$, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { useLocation } from '@builder.io/qwik-city';
+import { useLocation, useNavigate } from '@builder.io/qwik-city';
 
 export default component$(() => {
   const { url } = useLocation();
-  const params = new URLSearchParams(url.search);
-  const query = useSignal(params.get('q') ?? '');
+  const nav = useNavigate();
+  const query = useSignal(url.searchParams.get('q') ?? '');
 
   const oQueryChange = $((value: string) => {
     query.value = value;
+    url.searchParams.set('q', value);
+    nav(url.href);
   });
 
   return (
-    <div class="flex flex-col items-center gap-4 justify-items-center px-12 pb-8 pt-20 max-w-6xl relative mx-auto">
+    <div class="flex flex-col items-center gap-4 justify-items-center px-4 pb-8 pt-20 max-w-6xl relative mx-auto">
       <Hero />
 
       <div class="w-full max-w-2xl sticky top-10 z-10">
